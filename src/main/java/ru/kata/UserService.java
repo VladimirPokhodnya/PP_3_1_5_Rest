a77c4dfe7a0506f3588c2c08cc402f0bf30d5cf5
hammondy <hammondy@student.21-school.ru>
2023-04-15 08:41:41 +0700

    commit

---
+++ src/main/java/ru/kata/UserService.java
@@ -0,0 +1,29 @@
+package ru.kata;
+
+import ru.kata.model.User;
+import ru.kata.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repo;
+    public List<User> listAll() {
+        return repo.listUser();
+    }
+
+    public void save(User user) {
+        repo.save(user);
+    }
+
+    public User get(Long id) {
+        return repo.findById(id).get();
+    }
+
+    public void delete(Long id) {
+        repo.deleteById(id);
+    }
+}
---
+++ src/main/java/ru/kata/controller/AppController.java
@@ -0,0 +1,69 @@
+package ru.kata.controller;
+
+import java.util.List;
+
+import ru.kata.UserService;
+import ru.kata.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AppController {
+    private final UserService userService;
+
+    public AppController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String viewUser(Model model) {
+        List<User> listUsers = userService.listAll();
+        model.addAttribute("listUsers", listUsers);
+
+        return "admin";
+    }
+
+    @RequestMapping("/add")
+    public String showNewUserForm(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+
+        return "new_user";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.save(user);
+
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.save(user);
+
+        return "redirect:/";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditUserForm(@PathVariable(name = "id") Long id) {
+        ModelAndView mav = new ModelAndView("edit_user");
+
+        User user = userService.get(id);
+        mav.addObject("user", user);
+
+        return mav;
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteUser(@PathVariable(name = "id") Long id) {
+        userService.delete(id);
+
+        return "redirect:/";
+    }
+}
